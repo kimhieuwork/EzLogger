5229caa63ebd16ad5cfe26ac95500a8b5023eec7
PC19 <PC19@nstzdh.com>
2025-10-17 09:18:48 +0700

    first commit

---
+++ LogViewerControl.Designer.cs
@@ -0,0 +1,59 @@
+﻿
+namespace EzLogger
+{
+    partial class LogViewerControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._rtb = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // _rtb
+            // 
+            this._rtb.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._rtb.Location = new System.Drawing.Point(0, 0);
+            this._rtb.Name = "_rtb";
+            this._rtb.Size = new System.Drawing.Size(700, 300);
+            this._rtb.TabIndex = 0;
+            this._rtb.Text = "";
+            // 
+            // LogViewerControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this._rtb);
+            this.Name = "LogViewerControl";
+            this.Size = new System.Drawing.Size(700, 300);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox _rtb;
+    }
+}
---
+++ LogViewerControl.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EzLogger
+{
+    public partial class LogViewerControl : UserControl
+    {
+        public LogViewerControl()
+        {
+            InitializeComponent();
+        }
+        public void Log(string message, LogLevel level = LogLevel.Info)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action(() => Log(message, level)));
+                return;
+            }
+
+            Color color = Color.White;
+            switch (level)
+            {
+                case LogLevel.Info:
+                    color = Color.Green;
+                    break;
+                case LogLevel.Warning:
+                    color = Color.Yellow;
+                    break;
+                case LogLevel.Error:
+                    color = Color.Red;
+                    break;
+                case LogLevel.Debug:
+                    color = Color.Blue;
+                    break;
+            }
+
+            _rtb.SelectionStart = _rtb.TextLength;
+            _rtb.SelectionColor = color;
+            _rtb.AppendText($"{DateTime.Now:HH:mm:ss} [{level}] {message}\n");
+            _rtb.ScrollToCaret();
+        }
+    }
+}
